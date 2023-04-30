327a4b6d27c1b132dcdee701a878d6a00c2872f8
harsh-1503 <harshkarwa1345@gmail.com>
2023-04-30 15:44:17 +0530

    'update'

--- index.js
+++ index.js
@@ -1,72 +1,110 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const oracledb = require('oracledb')
-require('dotenv').config();
+const oracledb = require("oracledb");
+require("dotenv").config();
 // const {connectoracle} = require('./db')
 // connectoracle();
 // const {cons} = require('./db')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-const path = require('path')
+const path = require("path");
 const template_path = path.join(__dirname, "./template/views");
 console.log(__dirname);
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", template_path);
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
+// app.get("/saveRecipe",(req,res)=>{
+//   res.render("recipes") 
+// })
+app.post("/saveRecipe", async (req, res) => {
+  let cons;
+  const {
+    recipename,
+    instructions,
+    ingredients,
+    cookTime,
+    prepTime,
+    servings,
+  } = req.body;
+  try {
+    cons = await oracledb.getConnection({
+      user: process.env.USER,
+      password: process.env.PASS,
+      connectString: process.env.CONNECT_STRING,
+    });
 
-app.get('/',(req,res)=>{
-    res.render("index")
-})
+    const data = await cons.execute(`SELECT * FROM foodrecipe`);
 
-app.post('/saveRecipe',async(req,res)=>{
-    let cons;
-    const {recipename,instructions,ingredients,cookTime,prepTime,servings} = req.body;
-    try {
-        cons = await oracledb.getConnection({     
-            user:process.env.USER,
-            password:process.env.PASS,  
-            connectString: process.env.CONNECT_STRING,
-        });
+    console.log(data.rows);
+    console.log("Connected Successfully");
+    // console.log(req);
+    res.redirect('saveRecipe')
+    // console.log(name+" "+email+" "+password)
+  } catch (error) {
+    console.log("Oracle not connected");
+    console.log(error);
+    return;
+  }
 
-        const data = await cons.execute(
-            `SELECT * FROM foodrecipe`
-        )
+  try {
+    const addRecipe = await cons.execute(
+      `INSERT INTO foodrecipe
+        VALUES (:recipename,:ingredients,:instructions,:prepTime,:cookTime,:servings)`,
+      { recipename, ingredients, instructions, cookTime, prepTime, servings }
+    );
+    await cons.commit();
+    console.log("Recipe added Successfully");
+    console.log(addRecipe);
+    // alert('Recipe Added Successfully')
+  } catch (error) {
+    console.log("Recipe not added:" + error);
+  }
+});
 
-        console.log(data.rows);
-        console.log('Connected Successfully');
-        // console.log(req);
-        
 
-        // console.log(name+" "+email+" "+password)
-          
-    } catch (error) {
-        console.log('Oracle not connected');
-        console.log(error);
-        return
+
+// Set the view engine to EJS
+// app.set('view engine', 'ejs');
+
+// Connect to the OracleDB
+oracledb.getConnection(
+  {
+    user: process.env.USER,
+    password: process.env.PASS,
+    connectString: process.env.CONNECT_STRING,
+  },
+  (err, connection) => {
+    if (err) {
+      console.error(err.message);
+      return;
     }
 
-    try {
-        const addRecipe = await cons.execute(`INSERT INTO foodrecipe
-        VALUES (:recipename,:ingredients,:instructions,:prepTime,:cookTime,:servings)`,{recipename,ingredients,instructions,cookTime,prepTime,servings})
-        await cons.commit();
-        console.log('Recipe added Successfully');
-        console.log(addRecipe);
-    } catch (error) {
-        console.log("Recipe not added:" + error);
-    }
-    
 
-})
+    const sql = `SELECT * FROM foodrecipe`;
 
 
+    connection.execute(sql, [], (err, result) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
 
 
-app.listen(port,()=>{
-    console.log(`Server is listening at port ${port}`);
-})
+      connection.release();
+
+      app.get('/saveRecipe', (req, res) => {
+        // console.log(result.rows)
+        res.render('recipes', { data: result.rows });
+      });
+    });
+  }
+);
 
 
-
-
-
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}`);
+});
